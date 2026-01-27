1e5467d5e589628894e54cd2b5c983488ba73d4b
Drakowkq <da231105@gmail.com>
2026-01-27 09:56:46 +0700

    change clone() to iterators

--- greppy/src/lib.rs
+++ greppy/src/lib.rs
@@ -11,12 +11,19 @@ pub struct Config {
 
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str>{
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str>{
         if args.len() < 3 {
             return Err("not enough args");
         }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next(){
+            Some(value) => value,
+            _ => return Err("failed to parse args")
+        };
+
+        let filename = match args.next() {
+            Some(value) => value,
+            _ => return Err("failed to parse args")
+        };
 
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
@@ -45,26 +52,11 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line);
-        }
-    }
-    result
+    contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents.lines().filter(|line| line.to_lowercase().contains(&query.to_lowercase())).collect()
 }
 
 
--- greppy/src/main.rs
+++ greppy/src/main.rs
@@ -5,18 +5,15 @@ use greppy::Config;
 
 /*
     TODO: 
-        1)поменять clone на что-то более эффективное
-        2) добавить разбор регулярных выражений???
-        3) мб иначе завершпть програму?)
+        1) добавить разбор регулярных выражений???
 */
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Failed to parse config {}", err);
         process::exit(1);
     });
-    println!("{:?}", args);
+
     println!("Searching for {}", config.query);
     println!("in file {}", config.filename);
 
